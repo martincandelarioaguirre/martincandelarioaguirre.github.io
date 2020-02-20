ed1271c16e32230b8c36c6a93e772761a36b7080
Martin Candelario(UST <145028@ust-global.com>
2020-02-20 01:13:12 -0600

    professional section added and new object to angular object to add info dynamically

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -7,6 +7,15 @@ app.controller("appController", ['$scope',
     function ($scope) {
         $scope.hello = 'Hello World Martin';
 
+        $scope.personalInformation = {
+            Name: "Martin Candelario Aguirre",
+            BirthDay: new Date("1992-02-10"),
+            Age: new Date("1992-02-10"),
+            YearsOfExperience: new Date("2014-06-01"),
+            Location: "Guadalajara, Jalisco, Mexico",
+            Degree: "IT Engineer"
+        }
+
         $scope.jobs = [
             {
                 CompanyName: "UST-GLOBAL",
@@ -66,5 +75,16 @@ app.controller("appController", ['$scope',
             }
         ];
 
+        $scope.calculateAge = function(birthday) { // birthday is a date
+            var ageDifMs = Date.now() - birthday.getTime();
+            var ageDate = new Date(ageDifMs); // miliseconds from epoch
+            return Math.abs(ageDate.getUTCFullYear() - 1970);
+        }
+
+        $scope.calculateExperience = function(startDate) {
+            var difdt = new Date(new Date() - startDate);
+
+            return((difdt.toISOString().slice(0, 4) - 1970) + " Years " + (difdt.getMonth()+1) + " Months ");
+        }
     }
 ]);
