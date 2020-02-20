5a03c69f2176c0915836fe4d7a2810308ce32416
Martin Candelario(UST <145028@ust-global.com>
2020-02-19 23:42:37 -0600

    new files added with new style to online cv

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -6,5 +6,65 @@ var app = angular.module("app", []);
 app.controller("appController", ['$scope',
     function ($scope) {
         $scope.hello = 'Hello World Martin';
+
+        $scope.jobs = [
+            {
+                CompanyName: "UST-GLOBAL",
+                CompanyLocation: "CompanyLocation",
+                PeriodOfTime: "PeriodOfTime",
+                Profile: "Profile of the position",
+                Description: "Description of the job",
+                Tools: "Tools used by project"
+            },
+            {
+                CompanyName: "Tiempo Development",
+                CompanyLocation: "CompanyLocation",
+                PeriodOfTime: "PeriodOfTime",
+                Profile: "Profile of the position",
+                Description: "Description of the job",
+                Tools: "Tools used by project"
+            },
+            {
+                CompanyName: "Softtek",
+                CompanyLocation: "CompanyLocation",
+                PeriodOfTime: "PeriodOfTime",
+                Profile: "Profile of the position",
+                Description: "Description of the job",
+                Tools: "Tools used by project"
+            },
+            {
+                CompanyName: "Al Maximo TI",
+                CompanyLocation: "CompanyLocation",
+                PeriodOfTime: "PeriodOfTime",
+                Profile: "Profile of the position",
+                Description: "Description of the job",
+                Tools: "Tools used by project"
+            },
+            {
+                CompanyName: "Mach Tecnologia",
+                CompanyLocation: "CompanyLocation",
+                PeriodOfTime: "PeriodOfTime",
+                Profile: "Profile of the position",
+                Description: "Description of the job",
+                Tools: "Tools used by project"
+            },
+            {
+                CompanyName: "Intekel Automatizacion",
+                CompanyLocation: "CompanyLocation",
+                PeriodOfTime: "PeriodOfTime",
+                Profile: "Profile of the position",
+                Description: "Description of the job",
+                Tools: "Tools used by project"
+            },
+            {
+                CompanyName: "Etileno XXI",
+                CompanyLocation: "CompanyLocation",
+                PeriodOfTime: "PeriodOfTime",
+                Profile: "Profile of the position",
+                Description: "Description of the job",
+                Tools: "Tools used by project"
+            }
+        ];
+
     }
 ]);
