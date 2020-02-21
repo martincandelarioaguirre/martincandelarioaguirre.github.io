eef3fa65e55bf557c198c7bae65847d6b41df653
Martin Candelario(UST <145028@ust-global.com>
2020-02-20 20:39:01 -0600

    Changes of the template

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -13,7 +13,12 @@ app.controller("appController", ['$scope',
             Age: new Date("1992-02-10"),
             YearsOfExperience: new Date("2014-06-01"),
             Location: "Guadalajara, Jalisco, Mexico",
-            Degree: "IT Engineer"
+            Degree: "IT Engineer",
+            CareerLevel: "Software Developer",
+            Phone: "### ### ####",
+            Email: "m.c.a._92@hotmail.com",
+            Passport: "Yes",
+            Resume: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it      look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident,      sometimes on purpose (injected humour and the like)."
         }
 
         $scope.jobs = [
